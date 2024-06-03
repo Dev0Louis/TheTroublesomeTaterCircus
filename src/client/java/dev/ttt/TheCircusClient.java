522e9d3bb34c4ebc033457d149ebbbad9c2fc9da
D1p4k <82884007+D1p4k@users.noreply.github.com>
2024-06-03 22:21:08 +0200

    Setup I guess?

--- src/client/java/dev/ttt/TheCircusClient.java
+++ src/client/java/dev/ttt/TheCircusClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package dev.ttt;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class TheCircusClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/dev/ttt/mixin/ExampleMixin.java
+++ src/main/java/dev/ttt/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package dev.ttt.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
